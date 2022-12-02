c990e3456f044f145deaebe63340b7e442cfc0e4
Ralica1 <rasim.korajlic@gmail.com>
2022-12-03 00:38:38 +0100

    Finished 7. and 8. exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+let sum=0;
+return sum=num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	let sum=0;
+  return sum=num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum=0;
+  if (array==false) {
+    return sum=0;
+  }
+  else{
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function multiply(array) {
+  if (array.length === 0) {
+    return 1;
+  }
+  return array[0] * multiply(array.slice(1));
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function fact(number) {
+  const f=[];
+	if (number == 0 || number == 1)
+    return 1;
+  if (f[number] > 0)
+    return f[number];
+  return f[number] = fact(number-1) * number;
 };
 
 // Do not edit below this line
